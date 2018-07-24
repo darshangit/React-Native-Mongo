4b753332e5f3faac28cf116032cf57b255147054
darshangit <darshans1989@gmail.com>
2018-07-24 20:44:19 +0530

    flat list added

--- App.js
+++ App.js
@@ -12,26 +12,32 @@ export default class App extends React.Component {
   placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(placeName)
+        places: prevState.places.concat({
+          key: Math.random(),
+          value: placeName
+        })
       };
     });
   };
 
-  placeDeletedHandler = index => {
+  placeDeletedHandler = key => {
     this.setState(prevState => {
-      return  {
-        places: prevState.places.filter((place, i) => {
-          return i !== index;
+      return {
+        places: prevState.places.filter((place) => {
+          return place.key !== key;
         })
       };
-    })
-  }
+    });
+  };
 
   render() {
     return (
       <View style={styles.container}>
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList places={this.state.places} onItemDeleted={this.placeDeletedHandler}/>
+        <PlaceList
+          places={this.state.places}
+          onItemDeleted={this.placeDeletedHandler}
+        />
       </View>
     );
   }
